cf75c37d4618e003db5feefbc8f9f56c62f4833e
Peter Sarll <petersarll@gmail.com>
2020-04-25 16:33:13 -0700

    adding components, adding icons, adding stylization

--- workout_app/src/App.js
+++ workout_app/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import HomePage from './pages/HomePage/HomePage.component'
 import Header from './components/Header/Header.component.jsx'
+import WorkoutPhoto from './homepage.jpg'
 
-import './App.css'
+import './App.scss'
 
 function App() {
   return (
     <div className="App">
+      <img className="workout-photo" src={WorkoutPhoto} alt="workout" />
       <Header />
       <HomePage />
     </div>
--- workout_app/src/components/Header/Header.component.jsx
+++ workout_app/src/components/Header/Header.component.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
+import { Menu, LogOut, User } from 'react-feather'
+
 import './Header.styles.scss'
+
+//change classnames on all span to center texts in div
 
 function Header() {
   return (
     <div className="header">
-      <span className="header-menu">Menu</span>
+      <div className="menu-icon" id="icon-container">
+        <Menu />
+        <span className="header">Menu</span>
+      </div>
+      <div className="sign-in-icon" id="icon-container">
+        <User />
+        <span className="sign-in-header">Profile</span>
+      </div>
+      <div className="sign-out-icon" id="icon-container">
+        <LogOut />
+        <span className="sign-out-header">Logout</span>
+      </div>
     </div>
   )
 }
