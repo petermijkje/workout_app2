94a4a66ded9d9929e40f948aa92b14801d23c99d
Peter Sarll <petersarll@gmail.com>
2020-04-25 23:05:38 -0700

    adding individual stylization

--- workout_app/src/components/Header/Header.component.jsx
+++ workout_app/src/components/Header/Header.component.jsx
@@ -3,22 +3,22 @@ import { Menu, LogOut, User } from 'react-feather'
 
 import './Header.styles.scss'
 
-//change classnames on all span to center texts in div
+//change classnames on all
 
 function Header() {
   return (
     <div className="header">
       <div className="menu-icon" id="icon-container">
         <Menu />
-        <span className="header">Menu</span>
+        <span className="header-text">Menu</span>
       </div>
       <div className="sign-in-icon" id="icon-container">
         <User />
-        <span className="sign-in-header">Profile</span>
+        <span className="header-text">Profile</span>
       </div>
       <div className="sign-out-icon" id="icon-container">
         <LogOut />
-        <span className="sign-out-header">Logout</span>
+        <span className="header-text">Logout</span>
       </div>
     </div>
   )
