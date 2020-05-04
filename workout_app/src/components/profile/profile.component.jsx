b71f1237554c71aa33b883beea052d7351b46498
Peter Sarll <petersarll@gmail.com>
2020-05-03 21:26:28 -0700

    adding profile

--- workout_app/src/components/profile/profile.component.jsx
+++ workout_app/src/components/profile/profile.component.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 import './profile.styles.scss'
 
 function Profile() {
-  return <div className="profile">Profile</div>
+  return <div className="profile">My Profile</div>
 }
 
 export default Profile
