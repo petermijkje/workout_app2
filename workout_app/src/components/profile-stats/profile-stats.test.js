6ae604ed43b9f90bfa94678b7a5c513190ebf641
Peter Sarll <petersarll@gmail.com>
2020-05-04 23:56:43 -0700

    adding testing to profile component

---
+++ workout_app/src/components/profile-stats/profile-stats.test.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import ProfileStats from './components/Header/Header.component.jsx'
+import { render } from '@testing-library/react'
+import { create } from 'react-test-renderer'
+
+import App from './App'
+
+describe('App', () => {
+  test('Renders', () => {
+    const {} = render(<App />)
+  })
+  test('renders workout app element', () => {
+    const { getByText } = render(<App />)
+    const linkElement = getByText(/workout app/i)
+    expect(linkElement).toBeInTheDocument()
+  })
+})
