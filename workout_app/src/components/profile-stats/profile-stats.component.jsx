6e5c6eec6106991dd79c2def754de68c74882a4a
Peter Sarll <petersarll@gmail.com>
2020-05-04 23:59:03 -0700

    creating profile stats component

--- workout_app/src/components/profile-stats/profile-stats.component.jsx
+++ workout_app/src/components/profile-stats/profile-stats.component.jsx
@@ -1,24 +1,9 @@
 import React from 'react'
 
-import './comp.scss'
+import './profile-stats.styles.scss'
 
-function App() {
-  return (
-    <Router>
-      <div className="App">
-        <Header />
-        <div className="content">
-          <Switch>
-            <Route exact path="/" component={HomePage} />
-            <Route path="/profile" component={Profile} />
-            <Route path="/Login" component={LoginAndLogout} />
-            <Route component={Error} />
-          </Switch>
-        </div>
-        <h1 style={{ textAlign: 'center' }}>workout App</h1>
-      </div>
-    </Router>
-  )
+function ProfileStats() {
+  return <div>Profile Stats Component</div>
 }
 
-export default App
+export default ProfileStats
