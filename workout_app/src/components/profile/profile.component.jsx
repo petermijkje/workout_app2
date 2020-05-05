56cd0c7c1f46082c5d567010a43795447d4ee264
Peter Sarll <petersarll@gmail.com>
2020-05-05 00:07:30 -0700

    creating profile stats component

--- workout_app/src/components/profile/profile.component.jsx
+++ workout_app/src/components/profile/profile.component.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import ProfileStats from '../profile-stats/profile-stats.component'
 
 import './profile.styles.scss'
 
 function Profile() {
-  return <div className="profile">My Profile</div>
+  return (
+    <div className="profile">
+      My Profile
+      <ProfileStats />
+    </div>
+  )
 }
 
 export default Profile
