4556262f7eac36d1386aeccf65eaa3460c480ddc
Peter Sarll <petersarll@gmail.com>
2020-05-06 22:59:33 -0700

    adding individual testing elements to profile stats component

--- workout_app/src/components/profile-stats/profile-stats.test.js
+++ workout_app/src/components/profile-stats/profile-stats.test.js
@@ -11,27 +11,27 @@ describe('ProfileStats', () => {
     const linkElement = getByText(/profile stats component/i)
     expect(linkElement).toBeInTheDocument()
   })
-  test('renders workout app element', () => {
+  test('renders workout app element Name', () => {
     const { getByText } = render(<ProfileStats />)
     const linkElement = getByText(/Name/i)
     expect(linkElement).toBeInTheDocument()
   })
-  test('renders workout app element', () => {
+  test('renders workout app element Age', () => {
     const { getByText } = render(<ProfileStats />)
     const linkElement = getByText(/Age/i)
     expect(linkElement).toBeInTheDocument()
   })
-  test('renders workout app element', () => {
+  test('renders workout app element Height', () => {
     const { getByText } = render(<ProfileStats />)
     const linkElement = getByText(/Height/i)
     expect(linkElement).toBeInTheDocument()
   })
-  test('renders workout app element', () => {
+  test('renders workout app element Sex', () => {
     const { getByText } = render(<ProfileStats />)
     const linkElement = getByText(/Sex/i)
     expect(linkElement).toBeInTheDocument()
   })
-  test('renders workout app element', () => {
+  test('renders workout app element Weight', () => {
     const { getByText } = render(<ProfileStats />)
     const linkElement = getByText(/Weight/i)
     expect(linkElement).toBeInTheDocument()
