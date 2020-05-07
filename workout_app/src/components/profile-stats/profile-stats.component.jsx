231ba0fdc21c67d5e7c6fa5bab40f80bb94b2adc
Peter Sarll <petersarll@gmail.com>
2020-05-06 22:55:14 -0700

    adding testing to components and adding stats to component

--- workout_app/src/components/profile-stats/profile-stats.component.jsx
+++ workout_app/src/components/profile-stats/profile-stats.component.jsx
@@ -3,7 +3,16 @@ import React from 'react'
 import './profile-stats.styles.scss'
 
 function ProfileStats() {
-  return <div>Profile Stats Component</div>
+  return (
+    <div>
+      Profile Stats Component
+      <span>Name</span>
+      <span>Age</span>
+      <span>Height</span>
+      <span>Sex</span>
+      <span>Weight</span>
+    </div>
+  )
 }
 
 export default ProfileStats
--- workout_app/src/components/profile-stats/profile-stats.test.js
+++ workout_app/src/components/profile-stats/profile-stats.test.js
@@ -11,4 +11,29 @@ describe('ProfileStats', () => {
     const linkElement = getByText(/profile stats component/i)
     expect(linkElement).toBeInTheDocument()
   })
+  test('renders workout app element', () => {
+    const { getByText } = render(<ProfileStats />)
+    const linkElement = getByText(/Name/i)
+    expect(linkElement).toBeInTheDocument()
+  })
+  test('renders workout app element', () => {
+    const { getByText } = render(<ProfileStats />)
+    const linkElement = getByText(/Age/i)
+    expect(linkElement).toBeInTheDocument()
+  })
+  test('renders workout app element', () => {
+    const { getByText } = render(<ProfileStats />)
+    const linkElement = getByText(/Height/i)
+    expect(linkElement).toBeInTheDocument()
+  })
+  test('renders workout app element', () => {
+    const { getByText } = render(<ProfileStats />)
+    const linkElement = getByText(/Sex/i)
+    expect(linkElement).toBeInTheDocument()
+  })
+  test('renders workout app element', () => {
+    const { getByText } = render(<ProfileStats />)
+    const linkElement = getByText(/Weight/i)
+    expect(linkElement).toBeInTheDocument()
+  })
 })
