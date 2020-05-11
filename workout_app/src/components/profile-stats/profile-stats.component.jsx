79abd3d373f28e4dd6d4a2c7a2c3fc93abf326a3
Peter Sarll <petersarll@gmail.com>
2020-05-10 23:39:30 -0700

    changing class component to hooks

--- workout_app/src/components/profile-stats/profile-stats.component.jsx
+++ workout_app/src/components/profile-stats/profile-stats.component.jsx
@@ -18,3 +18,23 @@ function ProfileStats() {
 }
 
 export default ProfileStats
+
+// function ProfileStats() {
+//     const [count, setCount] = useState(0)
+
+//     function handleAlertClick() {
+//       setTimeout(() => {
+//         alert('You clicked on: ' + count)
+//       }, 3000)
+//     }
+
+//     return (
+//       <div>
+//         <p>You clicked {count} times</p>
+//         <button onClick={() => setCount(count + 1)}>Click me</button>
+//         <button onClick={handleAlertClick}>Show alert</button>
+//       </div>
+//     )
+//   }
+
+//   export default ProfileStats
