d93942f3f9aa676599c0e0d92e8149a55bf27936
Peter Sarll <petersarll@gmail.com>
2020-05-16 21:00:41 -0700

    adding react-icons for better menu bar, redesigning component and building it through mobile first

--- workout_app/src/components/Header/Header.component.jsx
+++ workout_app/src/components/Header/Header.component.jsx
@@ -1,23 +1,37 @@
 import React from 'react'
-import { Menu } from 'react-feather'
 import { Link } from 'react-router-dom'
+import { IoMdMenu } from 'react-icons/io'
 
 import './Header.styles.scss'
 
 //change classnames on all
+function showMenu() {
+  return <div>contacts</div>
+}
 
-const Header = () => {
-  return (
-    <div className="header">
-      <Link to="/" className="menu-icon" id="icon-container">
-        <Menu color="black" />
-        <span className="header-text"></span>
-      </Link>
-      <Link to="/contact">
-        <span className="header-text">contact</span>
-      </Link>
-    </div>
-  )
+class Header extends React.Component {
+  constructor() {
+    super()
+    this.state = {}
+  }
+  render() {
+    return (
+      <div className="header">
+        <Link to="/" className="menu-icon" id="icon-container">
+          <IoMdMenu
+            color="black"
+            size="36px"
+            strokeWidth="3px"
+            onClick={showMenu}
+          />
+          <span className="header-text"></span>
+        </Link>
+        {/* <Link to="/contact">
+          <span className="header-text">contact</span>
+        </Link> */}
+      </div>
+    )
+  }
 }
 
 export default Header
