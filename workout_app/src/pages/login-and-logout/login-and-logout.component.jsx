df85f10c099d4127f083f3195962ddeffaa260ce
Peter Sarll <petersarll@gmail.com>
2020-05-18 21:51:18 -0700

    adding login function

--- workout_app/src/pages/login-and-logout/login-and-logout.component.jsx
+++ workout_app/src/pages/login-and-logout/login-and-logout.component.jsx
@@ -3,7 +3,11 @@ import React from 'react'
 import './login-and-logout.styles.scss'
 
 function Login() {
-  return <div className="login">Login Page</div>
+  return (
+    <div className="login">
+      Login Page<div className="login-div"></div>
+    </div>
+  )
 }
 
 export default Login
