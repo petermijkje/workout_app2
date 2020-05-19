c55fcde5f07bb2e04b41da211371ce9f63636df0
Peter Sarll <petersarll@gmail.com>
2020-05-18 22:05:18 -0700

    adding design to login

--- workout_app/src/pages/login-and-logout/login-and-logout.component.jsx
+++ workout_app/src/pages/login-and-logout/login-and-logout.component.jsx
@@ -5,7 +5,11 @@ import './login-and-logout.styles.scss'
 function Login() {
   return (
     <div className="login">
-      Login Page<div className="login-div"></div>
+      Login Page
+      <div className="login-div">
+        <div className="login-title">Log in</div>
+        <hr />
+      </div>
     </div>
   )
 }
