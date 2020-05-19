adf1d246ea3894e9bcb67337a9ab75526bb44c00
Peter Sarll <petersarll@gmail.com>
2020-05-18 22:11:21 -0700

    adding inputs to login

--- workout_app/src/pages/login-and-logout/login-and-logout.component.jsx
+++ workout_app/src/pages/login-and-logout/login-and-logout.component.jsx
@@ -5,10 +5,29 @@ import './login-and-logout.styles.scss'
 function Login() {
   return (
     <div className="login">
-      Login Page
       <div className="login-div">
         <div className="login-title">Log in</div>
-        <hr />
+        <form>
+          <label>
+            <input
+              className="email"
+              type="text"
+              name="email"
+              placeholder="Email"
+            />
+          </label>
+          <hr />
+          <label>
+            <input
+              className="password"
+              type="password"
+              name="email"
+              placeholder="Password"
+            />
+          </label>
+          <hr />
+          <input type="submit" value="Submit" />
+        </form>
       </div>
     </div>
   )
