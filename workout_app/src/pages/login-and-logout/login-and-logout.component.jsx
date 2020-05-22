53326e98de946fe3d2546fc5641a67eb0d308cf7
Peter Sarll <petersarll@gmail.com>
2020-05-21 20:00:03 -0700

    adding hook component to login

--- workout_app/src/pages/login-and-logout/login-and-logout.component.jsx
+++ workout_app/src/pages/login-and-logout/login-and-logout.component.jsx
@@ -1,14 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import CustomButton from '../../components/custom-button/custom-button.component'
 import { BsEyeSlashFill, BsEyeFill } from 'react-icons/bs'
 
 import './login-and-logout.styles.scss'
 import RetrainLogo from '../../components/retrain-logo/RetrainLogo.component'
 
-function eyeSlash() {
-  return <BsEyeSlashFill /> ? <BsEyeFill /> : <BsEyeSlashFill />
-}
 function Login() {
+  const eyeComponent = <BsEyeFill />
+  const eyeSlashComponent = <BsEyeSlashFill />
+  const [eyeSlashIcon, eyeSlash] = useState(eyeSlashComponent)
+  function handleClick() {
+    eyeSlash(
+      eyeSlashIcon === eyeSlashComponent ? eyeComponent : eyeSlashComponent
+    )
+  }
   return (
     <div className="login">
       <RetrainLogo />
@@ -33,10 +38,12 @@ function Login() {
             />
           </label>
           <hr className="hr-password" />
-          <BsEyeSlashFill className="eye-slash" />
-          <div>{eyeSlash}</div>
+          <div className="eye-slash" onClick={handleClick}>
+            {eyeSlashIcon}
+          </div>
         </form>
       </div>
+      <CustomButton />
     </div>
   )
 }
