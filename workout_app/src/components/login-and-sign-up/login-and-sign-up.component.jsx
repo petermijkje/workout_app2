b54fcf327ec014078fa42b3f14bb5e506781a88f
Peter Sarll <petersarll@gmail.com>
2020-05-28 10:41:19 -0700

    made login/ sign up component

---
+++ workout_app/src/components/login-and-sign-up/login-and-sign-up.component.jsx
@@ -0,0 +1,81 @@
+import React, { useState } from 'react'
+import CustomButton from '../../components/custom-button/custom-button.component'
+import { BsEyeSlashFill, BsEyeFill } from 'react-icons/bs'
+
+function LoginAndSignUp() {
+  //toggle between login and signup
+  const [login] = useState(false)
+
+  //toggle function/state for show/hide password
+  const [eyeSlashIcon, eyeSlash] = useState(false)
+  function handleEyeSlash() {
+    eyeSlash(!eyeSlashIcon)
+  }
+  //logic for email
+  const [email, setEmail] = useState('')
+  function handleEmail(e) {
+    setEmail(e.target.value)
+    console.log({ email })
+  }
+  //logic for password
+  const [password, setPassword] = useState('')
+  function handlePassword(e) {
+    setPassword(e.target.value)
+    console.log({ password })
+  }
+
+  return (
+    <div>
+      <div className="login-div">
+        <div className="login-title">
+          {login ? <div>Log in</div> : <div>Sign Up</div>}
+        </div>
+        <form className="form-inputs">
+          <label>
+            <input
+              className="email"
+              type="text"
+              name="email"
+              placeholder="email"
+              onChange={handleEmail}
+            />
+          </label>
+          <hr className="hr-email" />
+          {/* switch case to show or hide password input */}
+          <div className="eye-slash" onClick={handleEyeSlash}>
+            {eyeSlashIcon ? (
+              <div>
+                <label>
+                  <input
+                    className="password"
+                    type="password"
+                    name="email"
+                    placeholder="Password"
+                    onChange={handlePassword}
+                  />
+                </label>
+                <BsEyeSlashFill className="eye" />
+              </div>
+            ) : (
+              <div>
+                <label>
+                  <input
+                    className="password"
+                    type="text"
+                    name="email"
+                    placeholder="Password"
+                  />
+                </label>
+                <BsEyeFill className="eye" />
+              </div>
+            )}
+          </div>
+          <hr className="hr-password" />
+        </form>
+      </div>
+      <CustomButton />
+    </div>
+  )
+}
+
+export default LoginAndSignUp
--- workout_app/src/pages/login-and-logout/login-and-logout.component.jsx
+++ workout_app/src/pages/login-and-logout/login-and-logout.component.jsx
@@ -1,77 +1,14 @@
-import React, { useState } from 'react'
-import CustomButton from '../../components/custom-button/custom-button.component'
-import { BsEyeSlashFill, BsEyeFill } from 'react-icons/bs'
+import React from 'react'
 
 import './login-and-logout.styles.scss'
 import RetrainLogo from '../../components/retrain-logo/RetrainLogo.component'
+import LoginAndSignUp from '../../components/login-and-sign-up/login-and-sign-up.component'
 
 function Login() {
-  //toggle function/state for show/hide password
-  const [eyeSlashIcon, eyeSlash] = useState(true)
-  function handleEyeSlash() {
-    eyeSlash(!eyeSlashIcon)
-  }
-  //logic for email
-  const [email, setEmail] = useState('')
-  function handleEmail(e) {
-    setEmail(e.target.value)
-    console.log({ email })
-  }
-  //logic for password
-  const [password, setPassword] = useState('')
-  function handlePassword(e) {
-    setPassword(e.target.value)
-    console.log({ password })
-  }
   return (
     <div className="login">
       <RetrainLogo />
-      <div className="login-div">
-        <div className="login-title">Log in</div>
-        <form className="form-inputs">
-          <label>
-            <input
-              className="email"
-              type="text"
-              name="email"
-              placeholder="email"
-              onChange={handleEmail}
-            />
-          </label>
-          <hr className="hr-email" />
-          {/* switch case to show or hide password input */}
-          <div className="eye-slash" onClick={handleEyeSlash}>
-            {eyeSlashIcon ? (
-              <div>
-                <label>
-                  <input
-                    className="password"
-                    type="password"
-                    name="email"
-                    placeholder="Password"
-                    onChange={handlePassword}
-                  />
-                </label>
-                <BsEyeSlashFill className="eye" />
-              </div>
-            ) : (
-              <div>
-                <label>
-                  <input
-                    className="password"
-                    type="text"
-                    name="email"
-                    placeholder="Password"
-                  />
-                </label>
-                <BsEyeFill className="eye" />
-              </div>
-            )}
-          </div>
-          <hr className="hr-password" />
-        </form>
-      </div>
-      <CustomButton />
+      <LoginAndSignUp />
     </div>
   )
 }
