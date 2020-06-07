0e194bdd9e7400a0b503ab29bdfc4c7ec16b61d5
Peter Sarll <petersarll@gmail.com>
2020-06-07 16:44:29 -0700

    making true/false login component

--- workout_app/src/pages/login-and-logout/login-and-logout.component.jsx
+++ workout_app/src/pages/login-and-logout/login-and-logout.component.jsx
@@ -4,13 +4,28 @@ import './login-and-logout.styles.scss'
 import RetrainLogo from '../../components/retrain-logo/RetrainLogo.component'
 import LoginAndSignUp from '../../components/login-and-sign-up/login-and-sign-up.component'
 
-function Login() {
-  return (
-    <div className="login">
-      <RetrainLogo />
-      <LoginAndSignUp />
-    </div>
-  )
+class Login extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      isLoggedIn: false,
+    }
+  }
+  render() {
+    const { isLoggedIn } = this.state
+    return (
+      <div>
+        {isLoggedIn ? (
+          <div> main Page </div>
+        ) : (
+          <div className="login">
+            <RetrainLogo />
+            <LoginAndSignUp />
+          </div>
+        )}
+      </div>
+    )
+  }
 }
 
 export default Login
