73dac54c4e0d86c641a5a6bc84e070ab18d9f273
Peter Sarll <petersarll@gmail.com>
2020-06-07 22:21:00 -0700

    adding more functionality to the footer

--- workout_app/src/components/main-profile-page/main-profile-page.component.jsx
+++ workout_app/src/components/main-profile-page/main-profile-page.component.jsx
@@ -10,19 +10,48 @@ import './main-profile-page.styles.scss'
 class MainProfile extends React.Component {
   constructor() {
     super()
-    this.state = {}
+    this.state = {
+      home: true,
+      workout: false,
+      activity: false,
+      profile: false,
+    }
+    this.handleHome = this.handleHome.bind(this)
+    this.handleWorkout = this.handleWorkout.bind(this)
   }
+  handleHome() {
+    const { home } = this.state
+    this.setState({ home: !home })
+  }
+
+  handleWorkout() {
+    const { workout } = this.state
+    this.setState({ workout: !workout })
+  }
+
   render() {
+    const { home, workout } = this.state
     return (
       <div>
-        <Profile />
+        {home ? <Profile /> : null}
+        {workout ? <Profile /> : null}
         <div className="footer">
           <div className="icon-container">
             <div>
-              <img className="footer-icon" src={Home} alt="Home Icon" />
+              <img
+                className="footer-icon"
+                src={Home}
+                alt="Home Icon"
+                onClick={this.handleHome}
+              />
             </div>
             <div>
-              <img className="footer-icon" src={Dumbbell} alt="Dumbbell Icon" />
+              <img
+                className="footer-icon"
+                src={Dumbbell}
+                alt="Dumbbell Icon"
+                onClick={this.handleWorkout}
+              />
             </div>
             <div>
               <img className="footer-icon" src={Activity} alt="Activity Icon" />
