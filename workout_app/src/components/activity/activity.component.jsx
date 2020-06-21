c4b5a16f688ff2b6a608457629f28a234dba8062
Peter Sarll <petersarll@gmail.com>
2020-06-20 19:50:04 -0700

    adding the object of exercises into the activity component

--- workout_app/src/components/activity/activity.component.jsx
+++ workout_app/src/components/activity/activity.component.jsx
@@ -2,8 +2,29 @@ import React from 'react'
 
 import './activity.styles.scss'
 
+const pullExerciseDay = () => {
+  const workoutObject = {
+    date: {
+      exercise: 'Dumbbell Bench Press',
+      sets: 6,
+      reps: 8,
+      weight: 145,
+      rest: '1 min rest',
+    },
+  }
+  return (
+    <div>
+      <div>{workoutObject.date.exercise}</div>
+      <div>{workoutObject.date.sets}</div>
+      <div>{workoutObject.date.reps}</div>
+      <div>{workoutObject.date.weight}</div>
+      <div>{workoutObject.date.rest}</div>
+    </div>
+  )
+}
+
 function ActivityComponent() {
-  return <div className="activity-div"> this is the Activity Component</div>
+  return <div className="activity-div"> {pullExerciseDay()} </div>
 }
 
 export default ActivityComponent
