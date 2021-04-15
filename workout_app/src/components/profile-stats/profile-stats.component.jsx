0ddfd14c2940577c5accf23843d62f499fd7ba96
Peter Sarll <petersarll@gmail.com>
2021-04-14 23:19:47 -0700

    add best stats

--- workout_app/src/components/profile-stats/profile-stats.component.jsx
+++ workout_app/src/components/profile-stats/profile-stats.component.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
+import React from "react";
 
-import './profile-stats.styles.scss'
+import "./profile-stats.styles.scss";
 
 const ProfileStats = ({
   name,
@@ -20,7 +20,15 @@ const ProfileStats = ({
     <span>{weight}</span>
     <span>{currentGoal}</span>
     <span>{onTrack}</span>
+    <ul>
+      <li>Best Squat</li>
+      <li>Best Deadlift</li>
+      <li>Best Overhead Press</li>
+      <li>Best Bench Press</li>
+      <li>Best Run</li>
+      <li>Best Pull up</li>
+    </ul>
   </div>
-)
+);
 
-export default ProfileStats
+export default ProfileStats;
